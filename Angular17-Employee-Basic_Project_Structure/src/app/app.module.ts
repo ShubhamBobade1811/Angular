df7e43ccdb1050c7aaa317dac37300623a4cde7e
ShubhamBobade1811 <shubham.sdb18@gmail.com>
2024-04-20 13:49:50 +0530

    Added directive - backgroundColor Directive

--- Angular17-Employee-Basic_Project_Structure/src/app/app.module.ts
+++ Angular17-Employee-Basic_Project_Structure/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './pages/navigation/navigation.component';
+import { BackgroundColorDirective } from './shared/directives/background-color.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    BackgroundColorDirective
   ],
   imports: [
     BrowserModule,
